b56e5480ffff5e1d04cf9da5924b4abf47a770e9
Chase W. Nelson <cwnelson88@gmail.com>
2019-12-09 02:01:27 +0800

    Updated bootstrap script with extra options for min defined codons, bootstraps, cpus, and output to prepend

--- OLGenie_bootstrap.R
+++ OLGenie_bootstrap.R
@@ -1,21 +1,70 @@
 #! /usr/local/bin/Rscript --slave --no-restore --no-save
+# /nas3/cnelson/bin/anaconda2/bin/Rscript --slave --no-restore --no-save
 
 ############################################################################################################
 # OLGenie process codon results to bootstrap
 
 # Mac
-suppressMessages(library(package = tidyverse))
+suppressMessages(library(package = readr))
+suppressMessages(library(package = stringr))
 suppressMessages(library(package = boot))
 
 # At command line, call something like:
 # Rscript --quiet --no-restore --no-save script_file.R arg1 arg2 arg3 > results.out
 # Rscript --slave --quiet --no-restore --no-save OLGenie_process_codons_batch.R OLGenie_codon_results.txt 10000 > OLGenie_results.out
 
-# Gather command-line arguments
-argv <- commandArgs(trailingOnly = T)
-codon_results_file <- as.character(argv[1])
-NBOOTSTRAPS <- as.integer(argv[2]) # <- 10000
-NCPUS <- as.integer(argv[3]) # <- 4, 8, or 60
+############################################################################################################
+############################################################################################################
+### GATHER GLOBAL VARIABLES FROM COMMAND LINE
+ARGV <- commandArgs(trailingOnly = T)
+
+kill_script <- FALSE
+
+if(! (length(ARGV) >= 3)) {
+  kill_script <- TRUE
+} else if(! str_detect(string = ARGV[1], pattern = "\\w")) {
+  kill_script <- TRUE
+} else if(! str_detect(string = ARGV[2], pattern = "\\d")) {
+  kill_script <- TRUE
+} else if(! str_detect(string = ARGV[3], pattern = "\\d")) {
+  kill_script <- TRUE
+} # could add more conditions later
+
+if(kill_script) {
+  cat("\n\n### WARNING: there must be 3-6 command line arguments, in this order:\n")
+  cat("    (1) CODON RESULTS FILE\n")
+  cat("    (2) MINIMUM NUMBER OF DEFINED CODONS\n")
+  cat("    (3) NUMBER OF BOOTSTRAP REPLICATES\n")
+  cat("    (4) NUMBER OF CPUS (OPTIONAL)\n")
+  cat("    (5) STRING TO PREPEND TO OUTPUT LINES (OPTIONAL)\n\n")
+  quit(save = 'no', status = 1, runLast = TRUE)
+} #else {
+#  cat("\n\n### RUNNING! ###\n")
+#}
+
+CODON_RESULTS_FILE <- as.character(ARGV[1])
+MIN_DEFINED_CODONS <- as.integer(ARGV[2]) # 0, 6, or 8 # only used by OLGenie
+NBOOTSTRAPS <- as.integer(ARGV[3]) # <- 10000
+
+NCPUS <- 1
+if(! is.na(ARGV[4]) && str_detect(string = ARGV[4], pattern = "\\d") && ! str_detect(string = ARGV[4], pattern = "[a-zA-Z]")) {
+  NCPUS <- as.integer(ARGV[4]) # <- 4, 8, or 60
+}
+
+PREPEND_TO_OUTPUT <- ''
+if(! is.na(ARGV[5])) {
+  PREPEND_TO_OUTPUT <- as.character(ARGV[5])
+}
+
+# EXAMPLES
+#CODON_RESULTS_FILE <- "/Users/cwnelson88/Desktop/OLG/simulations/SIMULATIONS/OLGenie_codon_results.txt"
+#MIN_DEFINED_CODONS <- 6
+#NBOOTSTRAPS <- 100
+#NCPUS <- 4
+#PREPEND_TO_OUTPUT <- 'METADATA'
+
+# Read in the file
+suppressMessages(OLGenie_data <- read_tsv(file = CODON_RESULTS_FILE))
 
 
 ############################################################################################################
@@ -85,18 +134,26 @@ dNdS_diff_boot_fun <- function(codon_results, numerator, denominator, num_replic
 }
 
 # INPUT
-codon_results <- suppressMessages(read_tsv(codon_results_file))
+codon_results <- suppressMessages(read_tsv(CODON_RESULTS_FILE)) # 1,353 x 16
+
+# FILTER BY MIN NUMBER OF DEFINED CODONS
+codon_results <- codon_results[codon_results$num_defined_seqs >= MIN_DEFINED_CODONS, ] # 982 x 16
 
 # LEADING SUMMARY COLUMNS:
 NN_sites <- sum(codon_results$NN_sites, na.rm = T)
 SN_sites <- sum(codon_results$SN_sites, na.rm = T)
 NS_sites <- sum(codon_results$NS_sites, na.rm = T)
 SS_sites <- sum(codon_results$SS_sites, na.rm = T)
+
 summary_data <- paste(nrow(codon_results),
                       NN_sites, SN_sites, NS_sites, SS_sites, 
                       sum(codon_results$NN_diffs, na.rm = T), sum(codon_results$SN_diffs, na.rm = T), 
                       sum(codon_results$NS_diffs, na.rm = T), sum(codon_results$SS_diffs, na.rm = T), 
                       sep = "\t")
+
+if(PREPEND_TO_OUTPUT != '') {
+  summary_data <- paste(PREPEND_TO_OUTPUT, summary_data, sep = "\t")
+}
 
 # Determine which ORF1 ratio to use: dNN/dSN or dNS/dSS
 dNNdSN_ratio_used <- (min(NN_sites, SN_sites) > min(NS_sites, SS_sites))
@@ -110,9 +167,25 @@ boot_dNNdNS <- dNdS_diff_boot_fun(codon_results, 'NN', 'NS', NBOOTSTRAPS, NCPUS)
 boot_dNSdSS <- dNdS_diff_boot_fun(codon_results, 'NS', 'SS', NBOOTSTRAPS, NCPUS)
 boot_dSNdSS <- dNdS_diff_boot_fun(codon_results, 'SN', 'SS', NBOOTSTRAPS, NCPUS)
 
+# OUTPUT HEADER
+header <- c('num_codons', 'NN_sites', 'SN_sites', 'NS_sites', 'SS_sites', 'NN_diffs', 'SN_diffs', 'NS_diffs', 'SS_diffs', 
+            'ratio', 'site_rich_ratio', 'gene', 'num_replicates', 
+            'dN', 'dS', 'dNdS', 'dN_m_dS', 'boot_dN_SE', 'boot_dS_SE', 'boot_dN_over_dS_SE', 'boot_dN_over_dS_P', 'boot_dN_m_dS_SE', 'P_value')
+
+if(PREPEND_TO_OUTPUT != '') {
+  header <- c('METADATA', header)
+}
+
+cat(header, sep = "\t")
+cat("\n", sep = '')
+
 # OUTPUT ALL RESULTS
-cat(summary_data, 'dNNdSN', dNNdSN_ratio_used, 'ORF1', boot_dNNdSN, "\n", sep = "\t")
-cat(summary_data, 'dNNdNS', dNNdNS_ratio_used, 'ORF2', boot_dNNdNS, "\n", sep = "\t")
-cat(summary_data, 'dNSdSS', ! dNNdSN_ratio_used, 'ORF1', boot_dNSdSS, "\n", sep = "\t")
-cat(summary_data, 'dSNdSS', ! dNNdNS_ratio_used, 'ORF2', boot_dSNdSS, "\n", sep = "\t")
+cat(summary_data, 'dNNdSN', dNNdSN_ratio_used, 'ORF1', boot_dNNdSN, sep = "\t")
+cat("\n", sep = '')
+cat(summary_data, 'dNNdNS', dNNdNS_ratio_used, 'ORF2', boot_dNNdNS, sep = "\t")
+cat("\n", sep = '')
+cat(summary_data, 'dNSdSS', ! dNNdSN_ratio_used, 'ORF1', boot_dNSdSS, sep = "\t")
+cat("\n", sep = '')
+cat(summary_data, 'dSNdSS', ! dNNdNS_ratio_used, 'ORF2', boot_dSNdSS, sep = "\t")
+cat("\n", sep = '')
 
