16d074a9e7b97cb3dd4f5c91c544b1d8f4d0a885
Chase W. Nelson <cwnelson88@gmail.com>
2022-12-02 12:15:43 +0800

    Update OLGenie_bootstrap.R
    Updated for R version 4.2.*, which abolished back-compatibility for as.vector calls on tibble columns; instead, as.numeric(unlist()) is required

--- OLGenie_bootstrap.R
+++ OLGenie_bootstrap.R
@@ -75,11 +75,13 @@ if(! is.na(ARGV[6])) {
   PREPEND_TO_OUTPUT <- as.character(ARGV[6])
 }
 
-# EXAMPLES
-#CODON_RESULTS_FILE <- "XXX"
-#MIN_DEFINED_CODONS <- 6
+# EXAMPLES FOR LINE-BY-LINE DEBUGGING
+#CODON_RESULTS_FILE <- "~/Desktop/SARS-CoV-2/within_Sarbecovirus_n21/SARBECOVIRUS_functional_subsets/OLGenie/ORF3a/ss12_wP5L/OLGenie_codon_results.txt"
+#CODON_RESULTS_FILE <- "~/Desktop/SCIENCE/OLGenie/Oscar_example.txt"
+#MIN_DEFINED_CODONS <- 2
 #NBOOTSTRAPS <- 100
 #NCPUS <- 4
+#CORRECTION <- 'NONE'  # "JC"
 #PREPEND_TO_OUTPUT <- 'METADATA'
 
 # Read in the file
@@ -95,160 +97,170 @@ suppressMessages(OLGenie_data <- read_tsv(file = CODON_RESULTS_FILE))
 ############################################################################################################
 # BOOTSTRAP FUNCTION (dN - dS) for CODON UNIT, NO CORRECTION
 dNdS_diff_boot_fun <- function(codon_results, numerator, denominator, num_replicates, num_cpus) {
-  
-  # Function for dN
-  dN_function <- function(D, indices) {
-    dN <- sum(D[indices, paste0(numerator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(numerator, "_sites")], na.rm = TRUE)
-    return(dN)
-  }
-  
-  # Function for dN
-  dS_function <- function(D, indices) {
-    dS <- sum(D[indices, paste0(denominator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(denominator, "_sites")], na.rm = TRUE)
-    return(dS)
-  }
-  
-  # Function for dN - dS
-  dN_m_dS_function <- function(D, indices) {
-    dN <- sum(D[indices, paste0(numerator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(numerator, "_sites")], na.rm = TRUE)
-    dS <- sum(D[indices, paste0(denominator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(denominator, "_sites")], na.rm = TRUE)
-    dN_m_dS <- dN - dS
-    return(dN_m_dS)
-  }
-  
-  # Function for dN/dS
-  dN_over_dS_function <- function(D, indices) {
-    dN <- sum(D[indices, paste0(numerator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(numerator, "_sites")], na.rm = TRUE)
-    dS <- sum(D[indices, paste0(denominator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(denominator, "_sites")], na.rm = TRUE)
-    dN_over_dS <- dN / dS
-    return(dN_over_dS)
-  }
-  
-  # CREATE FUNCTION FOR dN/dS TO CALCULATE ITS SE
-  (dN <- sum(as.vector(codon_results[ , paste0(numerator, "_diffs")]), na.rm = TRUE) / sum(as.vector(codon_results[ , paste0(numerator, "_sites")]), na.rm = TRUE))
-  (dS <- sum(as.vector(codon_results[ , paste0(denominator, "_diffs")]), na.rm = TRUE) / sum(as.vector(codon_results[ , paste0(denominator, "_sites")]), na.rm = TRUE))
-  (dNdS <- dN / dS)
-  
-  # Run the BOOTSTRAPS
-  # boot dN
-  (boot_dN <- boot(data = codon_results, R = num_replicates, statistic = dN_function, parallel = 'multicore', ncpus = num_cpus))
-  (dN <- boot_dN$t0)
-  (boot_dN_SE <- sd(boot_dN$t))
-  
-  # boot dS
-  (boot_dS <- boot(data = codon_results, R = num_replicates, statistic = dS_function, parallel = 'multicore', ncpus = num_cpus))
-  (dS <- boot_dS$t0)
-  (boot_dS_SE <- sd(boot_dS$t))
-  
-  # boot dN - dS
-  (boot_dN_m_dS <- boot(data = codon_results, R = num_replicates, statistic = dN_m_dS_function, parallel = 'multicore', ncpus = num_cpus))
-  (dN_m_dS <- boot_dN_m_dS$t0)
-  (boot_dN_m_dS_SE <- sd(boot_dN_m_dS$t))
-  (boot_dN_m_dS_Z <- dN_m_dS / boot_dN_m_dS_SE)
-  (boot_dN_m_dS_P <- 2 * pnorm(-abs(boot_dN_m_dS_Z)))
-  
-  # boot dN/dS
-  (boot_dN_over_dS <- boot(data = codon_results, R = num_replicates, statistic = dN_over_dS_function, parallel = 'multicore', ncpus = num_cpus))
-  (dN_over_dS <- boot_dN_over_dS$t0)
-  (boot_dN_over_dS_SE <- sd(boot_dN_over_dS$t))
-  (boot_dN_over_dS_Z <- dN_over_dS / boot_dN_over_dS_SE)
-  (boot_dN_over_dS_P <- 2 * pnorm(-abs(boot_dN_over_dS_Z)))
-  
-  ### NEW: ASL (acheived significance level)
-  boot_dN_gt_dS_count <- sum(boot_dN_m_dS$t > 0)
-  boot_dN_eq_dS_count <- sum(boot_dN_m_dS$t == 0)
-  boot_dN_lt_dS_count <- sum(boot_dN_m_dS$t < 0)
-  ASL_dN_gt_dS_P <- boot_dN_lt_dS_count / (boot_dN_gt_dS_count + boot_dN_eq_dS_count + boot_dN_lt_dS_count)
-  ASL_dN_lt_dS_P <- boot_dN_gt_dS_count / (boot_dN_gt_dS_count + boot_dN_eq_dS_count + boot_dN_lt_dS_count)
-  
-  return(paste(num_replicates, dN, dS, dNdS, dN_m_dS, boot_dN_SE, boot_dS_SE, boot_dN_over_dS_SE, boot_dN_over_dS_P, 
-               boot_dN_m_dS_SE, boot_dN_m_dS_P, 
-               boot_dN_gt_dS_count, boot_dN_eq_dS_count, boot_dN_lt_dS_count, ASL_dN_gt_dS_P, ASL_dN_lt_dS_P,
-               sep = "\t"))
+ # DEBUGGING
+ #numerator <- 'NN'
+ #denominator <- 'NS'
+ #num_replicates <- 100
+ #num_cpus <- 4
+ 
+ # Function for dN
+ dN_function <- function(D, indices) {
+  dN <- sum(D[indices, paste0(numerator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(numerator, "_sites")], na.rm = TRUE)
+  return(dN)
+ }
+ 
+ # Function for dN
+ dS_function <- function(D, indices) {
+  dS <- sum(D[indices, paste0(denominator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(denominator, "_sites")], na.rm = TRUE)
+  return(dS)
+ }
+ 
+ # Function for dN - dS
+ dN_m_dS_function <- function(D, indices) {
+  dN <- sum(D[indices, paste0(numerator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(numerator, "_sites")], na.rm = TRUE)
+  dS <- sum(D[indices, paste0(denominator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(denominator, "_sites")], na.rm = TRUE)
+  dN_m_dS <- dN - dS
+  return(dN_m_dS)
+ }
+ 
+ # Function for dN/dS
+ dN_over_dS_function <- function(D, indices) {
+  dN <- sum(D[indices, paste0(numerator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(numerator, "_sites")], na.rm = TRUE)
+  dS <- sum(D[indices, paste0(denominator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(denominator, "_sites")], na.rm = TRUE)
+  dN_over_dS <- dN / dS
+  return(dN_over_dS)
+ }
+ 
+ # CREATE FUNCTION FOR dN/dS TO CALCULATE ITS SE
+ (dN <- sum(as.numeric(unlist(codon_results[ , paste0(numerator, "_diffs")])), na.rm = TRUE) / sum(as.numeric(unlist(codon_results[ , paste0(numerator, "_sites")])), na.rm = TRUE))
+ (dS <- sum(as.numeric(unlist(codon_results[ , paste0(denominator, "_diffs")])), na.rm = TRUE) / sum(as.numeric(unlist(codon_results[ , paste0(denominator, "_sites")])), na.rm = TRUE))
+ (dNdS <- dN / dS)
+ 
+ # Run the BOOTSTRAPS
+ # boot dN
+ (boot_dN <- boot(data = codon_results, R = num_replicates, statistic = dN_function, parallel = 'multicore', ncpus = num_cpus))
+ (dN <- boot_dN$t0)
+ (boot_dN_SE <- sd(boot_dN$t))
+ 
+ # boot dS
+ (boot_dS <- boot(data = codon_results, R = num_replicates, statistic = dS_function, parallel = 'multicore', ncpus = num_cpus))
+ (dS <- boot_dS$t0)
+ (boot_dS_SE <- sd(boot_dS$t))
+ 
+ # boot dN - dS
+ (boot_dN_m_dS <- boot(data = codon_results, R = num_replicates, statistic = dN_m_dS_function, parallel = 'multicore', ncpus = num_cpus))
+ (dN_m_dS <- boot_dN_m_dS$t0)
+ (boot_dN_m_dS_SE <- sd(boot_dN_m_dS$t))
+ (boot_dN_m_dS_Z <- dN_m_dS / boot_dN_m_dS_SE)
+ (boot_dN_m_dS_P <- 2 * pnorm(-abs(boot_dN_m_dS_Z)))
+ 
+ # boot dN/dS
+ (boot_dN_over_dS <- boot(data = codon_results, R = num_replicates, statistic = dN_over_dS_function, parallel = 'multicore', ncpus = num_cpus))
+ (dN_over_dS <- boot_dN_over_dS$t0)
+ (boot_dN_over_dS_SE <- sd(boot_dN_over_dS$t))
+ (boot_dN_over_dS_Z <- dN_over_dS / boot_dN_over_dS_SE)
+ (boot_dN_over_dS_P <- 2 * pnorm(-abs(boot_dN_over_dS_Z)))
+ 
+ ### NEW: ASL (acheived significance level)
+ boot_dN_gt_dS_count <- sum(boot_dN_m_dS$t > 0)
+ boot_dN_eq_dS_count <- sum(boot_dN_m_dS$t == 0)
+ boot_dN_lt_dS_count <- sum(boot_dN_m_dS$t < 0)
+ ASL_dN_gt_dS_P <- boot_dN_lt_dS_count / (boot_dN_gt_dS_count + boot_dN_eq_dS_count + boot_dN_lt_dS_count)
+ ASL_dN_lt_dS_P <- boot_dN_gt_dS_count / (boot_dN_gt_dS_count + boot_dN_eq_dS_count + boot_dN_lt_dS_count)
+ 
+ return(paste(num_replicates, dN, dS, dNdS, dN_m_dS, boot_dN_SE, boot_dS_SE, boot_dN_over_dS_SE, boot_dN_over_dS_P, 
+              boot_dN_m_dS_SE, boot_dN_m_dS_P, 
+              boot_dN_gt_dS_count, boot_dN_eq_dS_count, boot_dN_lt_dS_count, ASL_dN_gt_dS_P, ASL_dN_lt_dS_P,
+              sep = "\t"))
 }
 
 
 ############################################################################################################
 # BOOTSTRAP FUNCTION (dN - dS) for CODON UNIT, ***JUKES-CANTOR CORRECTION***
 dNdS_diff_boot_fun_JC <- function(codon_results, numerator, denominator, num_replicates, num_cpus) {
-  
-  # Function for dN
-  dN_function <- function(D, indices) {
-    dN <- sum(D[indices, paste0(numerator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(numerator, "_sites")], na.rm = TRUE)
-    dN <- -3/4 * log(1 - (4/3) * dN)
-    return(dN)
-  }
-  
-  # Function for dN
-  dS_function <- function(D, indices) {
-    dS <- sum(D[indices, paste0(denominator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(denominator, "_sites")], na.rm = TRUE)
-    dS <- -3/4 * log(1 - (4/3) * dS)
-    return(dS)
-  }
-  
-  # Function for dN - dS
-  dN_m_dS_function <- function(D, indices) {
-    dN <- sum(D[indices, paste0(numerator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(numerator, "_sites")], na.rm = TRUE)
-    dN <- -3/4 * log(1 - (4/3) * dN)
-    dS <- sum(D[indices, paste0(denominator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(denominator, "_sites")], na.rm = TRUE)
-    dS <- -3/4 * log(1 - (4/3) * dS)
-    dN_m_dS <- dN - dS
-    return(dN_m_dS)
-  }
-  
-  # Function for dN/dS
-  dN_over_dS_function <- function(D, indices) {
-    dN <- sum(D[indices, paste0(numerator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(numerator, "_sites")], na.rm = TRUE)
-    dN <- -3/4 * log(1 - (4/3) * dN)
-    dS <- sum(D[indices, paste0(denominator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(denominator, "_sites")], na.rm = TRUE)
-    dS <- -3/4 * log(1 - (4/3) * dS)
-    dN_over_dS <- dN / dS
-    return(dN_over_dS)
-  }
-  
-  # CREATE FUNCTION FOR dN/dS TO CALCULATE ITS SE
-  dN <- sum(as.vector(codon_results[ , paste0(numerator, "_diffs")]), na.rm = TRUE) / sum(as.vector(codon_results[ , paste0(numerator, "_sites")]), na.rm = TRUE)
+ # DEBUGGING
+ #numerator <- 'NN'
+ #denominator <- 'NS'
+ #num_replicates <- 100
+ #num_cpus <- 4
+ 
+ # Function for dN
+ dN_function <- function(D, indices) {
+  dN <- sum(D[indices, paste0(numerator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(numerator, "_sites")], na.rm = TRUE)
   dN <- -3/4 * log(1 - (4/3) * dN)
-  dS <- sum(as.vector(codon_results[ , paste0(denominator, "_diffs")]), na.rm = TRUE) / sum(as.vector(codon_results[ , paste0(denominator, "_sites")]), na.rm = TRUE)
+  return(dN)
+ }
+ 
+ # Function for dN
+ dS_function <- function(D, indices) {
+  dS <- sum(D[indices, paste0(denominator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(denominator, "_sites")], na.rm = TRUE)
   dS <- -3/4 * log(1 - (4/3) * dS)
-  dNdS <- dN / dS
-  
-  # Run the BOOTSTRAPS
-  # boot dN
-  (boot_dN <- boot(data = codon_results, R = num_replicates, statistic = dN_function, parallel = 'multicore', ncpus = num_cpus))
-  (dN <- boot_dN$t0)
-  (boot_dN_SE <- sd(boot_dN$t))
-  
-  # boot dS
-  (boot_dS <- boot(data = codon_results, R = num_replicates, statistic = dS_function, parallel = 'multicore', ncpus = num_cpus))
-  (dS <- boot_dS$t0)
-  (boot_dS_SE <- sd(boot_dS$t))
-  
-  # boot dN - dS
-  (boot_dN_m_dS <- boot(data = codon_results, R = num_replicates, statistic = dN_m_dS_function, parallel = 'multicore', ncpus = num_cpus))
-  (dN_m_dS <- boot_dN_m_dS$t0)
-  (boot_dN_m_dS_SE <- sd(boot_dN_m_dS$t))
-  (boot_dN_m_dS_Z <- dN_m_dS / boot_dN_m_dS_SE)
-  (boot_dN_m_dS_P <- 2 * pnorm(-abs(boot_dN_m_dS_Z)))
-  
-  # boot dN/dS
-  (boot_dN_over_dS <- boot(data = codon_results, R = num_replicates, statistic = dN_over_dS_function, parallel = 'multicore', ncpus = num_cpus))
-  (dN_over_dS <- boot_dN_over_dS$t0)
-  (boot_dN_over_dS_SE <- sd(boot_dN_over_dS$t))
-  (boot_dN_over_dS_Z <- dN_over_dS / boot_dN_over_dS_SE)
-  (boot_dN_over_dS_P <- 2 * pnorm(-abs(boot_dN_over_dS_Z)))
-  
-  ### NEW: ASL (acheived significance level)
-  boot_dN_gt_dS_count <- sum(boot_dN_m_dS$t > 0)
-  boot_dN_eq_dS_count <- sum(boot_dN_m_dS$t == 0)
-  boot_dN_lt_dS_count <- sum(boot_dN_m_dS$t < 0)
-  ASL_dN_gt_dS_P <- boot_dN_lt_dS_count / (boot_dN_gt_dS_count + boot_dN_eq_dS_count + boot_dN_lt_dS_count)
-  ASL_dN_lt_dS_P <- boot_dN_gt_dS_count / (boot_dN_gt_dS_count + boot_dN_eq_dS_count + boot_dN_lt_dS_count)
-  
-  return(paste(num_replicates, dN, dS, dNdS, dN_m_dS, boot_dN_SE, boot_dS_SE, boot_dN_over_dS_SE, boot_dN_over_dS_P, 
-               boot_dN_m_dS_SE, boot_dN_m_dS_P, 
-               boot_dN_gt_dS_count, boot_dN_eq_dS_count, boot_dN_lt_dS_count, ASL_dN_gt_dS_P, ASL_dN_lt_dS_P,
-               sep = "\t"))
+  return(dS)
+ }
+ 
+ # Function for dN - dS
+ dN_m_dS_function <- function(D, indices) {
+  dN <- sum(D[indices, paste0(numerator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(numerator, "_sites")], na.rm = TRUE)
+  dN <- -3/4 * log(1 - (4/3) * dN)
+  dS <- sum(D[indices, paste0(denominator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(denominator, "_sites")], na.rm = TRUE)
+  dS <- -3/4 * log(1 - (4/3) * dS)
+  dN_m_dS <- dN - dS
+  return(dN_m_dS)
+ }
+ 
+ # Function for dN/dS
+ dN_over_dS_function <- function(D, indices) {
+  dN <- sum(D[indices, paste0(numerator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(numerator, "_sites")], na.rm = TRUE)
+  dN <- -3/4 * log(1 - (4/3) * dN)
+  dS <- sum(D[indices, paste0(denominator, "_diffs")], na.rm = TRUE) / sum(D[indices, paste0(denominator, "_sites")], na.rm = TRUE)
+  dS <- -3/4 * log(1 - (4/3) * dS)
+  dN_over_dS <- dN / dS
+  return(dN_over_dS)
+ }
+ 
+ # CREATE FUNCTION FOR dN/dS TO CALCULATE ITS SE
+ dN <- sum(as.numeric(unlist(codon_results[ , paste0(numerator, "_diffs")])), na.rm = TRUE) / sum(as.numeric(unlist(codon_results[ , paste0(numerator, "_sites")])), na.rm = TRUE)
+ dN <- -3/4 * log(1 - (4/3) * dN)
+ dS <- sum(as.numeric(unlist(codon_results[ , paste0(denominator, "_diffs")])), na.rm = TRUE) / sum(as.numeric(unlist(codon_results[ , paste0(denominator, "_sites")])), na.rm = TRUE)
+ dS <- -3/4 * log(1 - (4/3) * dS)
+ dNdS <- dN / dS
+ 
+ # Run the BOOTSTRAPS
+ # boot dN
+ (boot_dN <- boot(data = codon_results, R = num_replicates, statistic = dN_function, parallel = 'multicore', ncpus = num_cpus))
+ (dN <- boot_dN$t0)
+ (boot_dN_SE <- sd(boot_dN$t))
+ 
+ # boot dS
+ (boot_dS <- boot(data = codon_results, R = num_replicates, statistic = dS_function, parallel = 'multicore', ncpus = num_cpus))
+ (dS <- boot_dS$t0)
+ (boot_dS_SE <- sd(boot_dS$t))
+ 
+ # boot dN - dS
+ (boot_dN_m_dS <- boot(data = codon_results, R = num_replicates, statistic = dN_m_dS_function, parallel = 'multicore', ncpus = num_cpus))
+ (dN_m_dS <- boot_dN_m_dS$t0)
+ (boot_dN_m_dS_SE <- sd(boot_dN_m_dS$t))
+ (boot_dN_m_dS_Z <- dN_m_dS / boot_dN_m_dS_SE)
+ (boot_dN_m_dS_P <- 2 * pnorm(-abs(boot_dN_m_dS_Z)))
+ 
+ # boot dN/dS
+ (boot_dN_over_dS <- boot(data = codon_results, R = num_replicates, statistic = dN_over_dS_function, parallel = 'multicore', ncpus = num_cpus))
+ (dN_over_dS <- boot_dN_over_dS$t0)
+ (boot_dN_over_dS_SE <- sd(boot_dN_over_dS$t))
+ (boot_dN_over_dS_Z <- dN_over_dS / boot_dN_over_dS_SE)
+ (boot_dN_over_dS_P <- 2 * pnorm(-abs(boot_dN_over_dS_Z)))
+ 
+ ### NEW: ASL (acheived significance level)
+ boot_dN_gt_dS_count <- sum(boot_dN_m_dS$t > 0)
+ boot_dN_eq_dS_count <- sum(boot_dN_m_dS$t == 0)
+ boot_dN_lt_dS_count <- sum(boot_dN_m_dS$t < 0)
+ ASL_dN_gt_dS_P <- boot_dN_lt_dS_count / (boot_dN_gt_dS_count + boot_dN_eq_dS_count + boot_dN_lt_dS_count)
+ ASL_dN_lt_dS_P <- boot_dN_gt_dS_count / (boot_dN_gt_dS_count + boot_dN_eq_dS_count + boot_dN_lt_dS_count)
+ 
+ return(paste(num_replicates, dN, dS, dNdS, dN_m_dS, boot_dN_SE, boot_dS_SE, boot_dN_over_dS_SE, boot_dN_over_dS_P, 
+              boot_dN_m_dS_SE, boot_dN_m_dS_P, 
+              boot_dN_gt_dS_count, boot_dN_eq_dS_count, boot_dN_lt_dS_count, ASL_dN_gt_dS_P, ASL_dN_lt_dS_P,
+              sep = "\t"))
 }
 
 ############################################################################################################
@@ -265,7 +277,8 @@ SN_sites <- sum(codon_results$SN_sites, na.rm = T)
 NS_sites <- sum(codon_results$NS_sites, na.rm = T)
 SS_sites <- sum(codon_results$SS_sites, na.rm = T)
 
-summary_data <- paste(nrow(codon_results),
+summary_data <- paste(CODON_RESULTS_FILE,
+                      nrow(codon_results),
                       NN_sites, SN_sites, NS_sites, SS_sites, 
                       sum(codon_results$NN_diffs, na.rm = T), sum(codon_results$SN_diffs, na.rm = T), 
                       sum(codon_results$NS_diffs, na.rm = T), sum(codon_results$SS_diffs, na.rm = T), 
@@ -317,7 +330,7 @@ if(CORRECTION == "JC") {
 #[14] boot_dN_lt_dS_count
 #[15] ASL_dN_gt_dS_P
 #[16] ASL_dN_lt_dS_P
-header <- c('num_codons', 'NN_sites', 'SN_sites', 'NS_sites', 'SS_sites', 'NN_diffs', 'SN_diffs', 'NS_diffs', 'SS_diffs', 
+header <- c('file', 'num_codons', 'NN_sites', 'SN_sites', 'NS_sites', 'SS_sites', 'NN_diffs', 'SN_diffs', 'NS_diffs', 'SS_diffs', 
             'ratio', 'site_rich_ratio', 'gene', 'num_replicates', 
             'dN', 'dS', 'dNdS', 'dN_m_dS', 'boot_dN_SE', 'boot_dS_SE', 'boot_dN_over_dS_SE', 'boot_dN_over_dS_P', 'boot_dN_m_dS_SE', 'boot_dN_m_dS_P',
             'boot_dN_gt_dS_count', 'boot_dN_eq_dS_count', 'boot_dN_lt_dS_count', 'ASL_dN_gt_dS_P', 'ASL_dN_lt_dS_P')
